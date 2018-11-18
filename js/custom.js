682f3e4fc92bcfd7a389403f0ef3b6e226026df4
memoirsofmen <44543957+memoirsofmen@users.noreply.github.com>
2018-11-18 18:08:09 +0400

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,74 +1,60 @@
-$(document).ready(function() {
+    $(document).ready(function() {
 
 
-    var userFeed = new Instafeed({
+        var userFeed = new Instafeed({
 
 
 
 
 
-        get: 'user',
-        userId: '6935940613',
-        limit: 100,
-        resolution: 'standard_resolution',
-        accessToken: '6935940613.1677ed0.9496fd5d8d4a48ed9a282332e2b7909b',
-        sortBy: 'most-commented',
-        filter: function(image) {
-      var MAX_LENGTH = 10;
+            get: 'user',
+            userId: '6935940613',
+            limit: 100,
+            resolution: 'standard_resolution',
+            accessToken: '6935940613.1677ed0.9496fd5d8d4a48ed9a282332e2b7909b',
+            sortBy: 'most-commented',
+              template: '<div class="col-lg-4 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+       
+        
 
-      // here we create a property called "short_caption"
-      // on the image object, using the original caption
-      if (image.caption && image.caption.text) {
-        image.short_caption = image.caption.text.slice(0, MAX_LENGTH);
-      } else {
-        image.short_caption = "";
+
+
+
+        });
+
+
+        var feed = new Instafeed({
+      // other options...
+      filter: function(image) {
+        var newCaption = [];
+        var captionParts;
+
+        if (image.caption && image.caption.text && image.caption.text.length > 0) {
+          captionParts = image.caption.text.split(' ');
+          for (var i=0; i < captionParts.length ; i++) {
+            if (captionParts[i].charAt(0) !== '#') {
+              newCaption.push(captionParts[i]);
+            }
+          }
+        }
+
+        image.caption_without_tags = newCaption.join(' ');
+
+        return true; // so we don't exclude any images
       }
+    });
 
-      // ensure the filter doesn't reject any images
-      return true;
-    },
-        template: '<div class="col-lg-4 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
-   
-    
+        userFeed.run();
 
-
+        
+        // This will create a single gallery from all elements that have class "gallery-item"
+        $('.gallery').magnificPopup({
+            type: 'image',
+            delegate: 'a',
+            gallery: {
+                enabled: true
+            }
+        });
 
 
     });
-
-
-    var feed = new Instafeed({
-  // other options...
-  filter: function(image) {
-    var newCaption = [];
-    var captionParts;
-
-    if (image.caption && image.caption.text && image.caption.text.length > 0) {
-      captionParts = image.caption.text.split(' ');
-      for (var i=0; i < captionParts.length ; i++) {
-        if (captionParts[i].charAt(0) !== '#') {
-          newCaption.push(captionParts[i]);
-        }
-      }
-    }
-
-    image.caption_without_tags = newCaption.join(' ');
-
-    return true; // so we don't exclude any images
-  }
-});
-
-    userFeed.run();
-
-    
-    // This will create a single gallery from all elements that have class "gallery-item"
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
-
-
-});
